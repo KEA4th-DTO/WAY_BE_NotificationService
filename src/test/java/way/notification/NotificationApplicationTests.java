fc5c32e7f0eb28896982373bb28e5b0772f92cbf
Haecheol Shin <dev.hc.shin@gmail.com>
2024-05-05 23:39:48 +0900

    publish repository

---
+++ src/test/java/way/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package way.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
