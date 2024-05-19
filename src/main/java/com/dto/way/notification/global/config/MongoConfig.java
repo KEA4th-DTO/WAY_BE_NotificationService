949cc727bc3fb95c36a3660149d6bc74935d0451
Haecheol Shin <dev.hc.shin@gmail.com>
2024-05-19 17:00:40 +0900

    DTO-285 Feat: Follow 알림 구현 완료
    - Follow 알림 구현을 완료했습니다.
    - Response코드를 추가했습니다.
    - CI/CD 구현을 위해 build폴더의 이그노어 설정을 해제했습니다.

--- src/main/java/com/dto/way/notification/global/config/MongoConfig.java
+++ src/main/java/com/dto/way/notification/global/config/MongoConfig.java
@@ -1,26 +1,48 @@
 package com.dto.way.notification.global.config;
 
-import lombok.RequiredArgsConstructor;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@RequiredArgsConstructor
 public class MongoConfig {
 
-    private final MongoMappingContext mongoMappingContext;
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
+
+    @Value("${MONGODB_DB_URI}")
+    private String mongoDBConnection;
 
     @Bean
-    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
-                                                       MongoMappingContext mongoMappingContext) {
-        DefaultDbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory mongoDatabaseFactory,
+            MongoMappingContext mongoMappingContext
+    ) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
         return converter;
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoDBConnection);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(settings);
     }
 }
--- src/main/java/com/dto/way/notification/web/controller/NotificationController.java
+++ src/main/java/com/dto/way/notification/web/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.dto.way.notification.web.controller;
 import com.dto.way.notification.domain.entity.Notification;
 import com.dto.way.notification.domain.service.EmitterService;
 import com.dto.way.notification.domain.service.NotificationService;
+import com.dto.way.notification.web.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -23,17 +24,21 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping(value = "/sse-connection", produces = "text/event-stream")
-    public SseEmitter stream(Authentication authentication,
-                      @RequestHeader(value = "Last-Event-Id", required = false, defaultValue = "") String lastEventId) {
+    public ApiResponse<SseEmitter> stream(Authentication authentication,
+                                         @RequestHeader(value = "Last-Event-Id", required = false, defaultValue = "") String lastEventId) {
 
         // 이메일 대신 memberId 들어가야 함
-        return emitterService.addEmitter(authentication.getName(), lastEventId);
+        SseEmitter sseEmitter = emitterService.addEmitter(authentication.getName(), lastEventId);
+        return ApiResponse.onSuccess(sseEmitter);
     }
 
     // memberID 가지고 알림 목록 조회 api
     @PostMapping("/notification-list")
-    public List<Notification>  readNotifications(@RequestBody String nickname) {
-        return notificationService.selectNotificationList(nickname);
+    public ApiResponse<List<Notification>> readNotifications(@RequestBody String nickname) {
+        // authentication에서 이메일로 유저정보를 찾고 거기서 닉네임을 찾아야한다.
+        // requestbody로 닉네임 받으면 안된다. 수정 필요.
+        List<Notification> notificationList = notificationService.selectNotificationList(nickname);
+        return ApiResponse.onSuccess(notificationList);
     }
 
 }
